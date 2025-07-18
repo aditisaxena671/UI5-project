5ae763edfa2825e3f6b1d090429475ac261ebddd
Aditi Saxena <aditisaxena671@gmail.com>
2025-07-18 12:15:59 +0000

    custom column display for UI table + custom hash on reload

--- webapp/controller/WorkFlow.controller.js
+++ webapp/controller/WorkFlow.controller.js
@@ -2,8 +2,11 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
     'sap/m/MessageBox',
+    'sap/ui/model/Filter',
+    "sap/ui/model/FilterOperator",
+    "sap/m/Dialog",
 
-], function (Controller, JSONModel, MessageBox) {
+], function (Controller, JSONModel, MessageBox, Filter, FilterOperator, Dialog) {
     "use strict";
     let _initialized = false;
     return Controller.extend("sap.ui.demo.walkthrough.project1.controller.WorkFlow", {
@@ -12,6 +15,7 @@ sap.ui.define([
                 console.log("Controller already initialized globally");
                 return;
             }
+            sap.ui.core.routing.HashChanger.getInstance().setHash("dashboard");
             _initialized = true;
             console.log("goinf to call api");
             this.fetchOrderData();
@@ -25,7 +29,7 @@ sap.ui.define([
                 });
             });
         },
-        
+
         fetchOrderData: function () {
             console.log("inside fetchOrderData")
             this.readOData("/Orders")
@@ -128,23 +132,117 @@ sap.ui.define([
             // this.customerDetailsDialog.unbindElement();
             this.customerDetailsDialog.close();
         },
-        onRowPress:function(oEvent){
+        onRowPress: function (oEvent) {
             console.log("Row Pressed");
-            var sPath= oEvent.getSource().getBindingContext("Northwind").getPath();
-            if(!this.customerDetailsDialog){
-                this.customerDetailsDialog= sap.ui.xmlfragment(this.getView().getId(),"sap.ui.demo.walkthrough.project1.view.CustomerDetail",this);
+            var sPath = oEvent.getSource().getBindingContext("Northwind").getPath();
+            if (!this.customerDetailsDialog) {
+                this.customerDetailsDialog = sap.ui.xmlfragment(this.getView().getId(), "sap.ui.demo.walkthrough.project1.view.CustomerDetail", this);
                 this.getView().addDependent(this.customerDetailsDialog);
             }
-            var oModel= this.getView().getModel("Northwind");
-            this.getView().byId("customerDetailsDialog").setModel(oModel,"Northwind");
+            var oModel = this.getView().getModel("Northwind");
+            this.getView().byId("customerDetailsDialog").setModel(oModel, "Northwind");
 
             this.getView().byId("customerDetailsDialog").bindElement({
-                path:sPath,
-                model:"Northwind",
+                path: sPath,
+                model: "Northwind",
                 parameters: { "expand": "Orders" }
             });
             this.getView().byId("customerDetailsDialog").getModel("Northwind").refresh(true);
             this.customerDetailsDialog.open();
+        },
+        onChange: function (oEvent) {
+            var sQuery = oEvent.getSource().getValue();
+            var searchFilter = [];
+            // console.log(sQuery);
+            if (sQuery && sQuery.length > 0) {
+                var filter1 = new Filter("ContactName", FilterOperator.Contains, sQuery);
+                var filter2 = new Filter("CompanyName", FilterOperator.Contains, sQuery);
+                var filter3 = new Filter("ContactTitle", FilterOperator.Contains, sQuery);
+                var filter4 = new Filter("Country", FilterOperator.Contains, sQuery);
+                var filter5 = new Filter("Phone", FilterOperator.Contains, sQuery);
+                var finalFilter = new Filter([filter1, filter2, filter3, filter4, filter5], false)
+                searchFilter.push(finalFilter);
+            }
+            var oTable = this.byId("cutomerTable");
+            var oBinding = oTable.getBinding("items");
+            oBinding.filter(searchFilter);
+        },
+        onHideOrShowColumnChange: function () {
+            if (!this.oColumnSelection) {
+                this.oColumnSelection = new Dialog({
+                    id: "hideShowDialog",
+                    title: "Select Columns to show",
+                    contentWidth: "550px",
+                    contentHeight: "300px",
+                    content: [
+                        new sap.m.List({
+                            id: "hideShowList",
+                            mode: "MultiSelect",
+                            items: [
+                                new sap.m.StandardListItem({
+                                    title: "Order Id",
+                                    selected: this.getView().byId("ShowDialogTable").getColumns()[0].getVisible()
+                                }),
+                                new sap.m.StandardListItem({
+                                    title: "Customer",
+                                    selected: this.getView().byId("ShowDialogTable").getColumns()[1].getVisible()
+                                }),
+                                new sap.m.StandardListItem({
+                                    title: "Order Date",
+                                    selected: this.getView().byId("ShowDialogTable").getColumns()[2].getVisible()
+                                }),
+                                new sap.m.StandardListItem({
+                                    title: "Ship City",
+                                    selected: this.getView().byId("ShowDialogTable").getColumns()[3].getVisible()
+                                }),
+                                new sap.m.StandardListItem({
+                                    title: "Shipped Date",
+                                    selected: this.getView().byId("ShowDialogTable").getColumns()[4].getVisible()
+                                })
+                            ]
+
+                        }),
+                        new sap.m.Button({
+                            text: "Reset",
+                            press: function () {
+                                var list = sap.ui.getCore().byId("hideShowList");
+                                list.getItems().forEach(function (items) {
+                                    items.setSelected(true);
+                                });
+                                var column = this.getView().byId("ShowDialogTable").getColumns();
+
+                                list.getItems().forEach(function (item, index) {
+                                    column[index].setVisible(item.getSelected());
+                                });
+                                this.oColumnSelection.close();
+                            }.bind(this)
+                        })
+
+
+                    ],
+                    beginButton: new sap.m.Button({
+                        text: "Apply",
+                        press: function () {
+                            // this.getView().byId("ShowDialogTable").getColumns()[0].setVisible(this.getView().byId(hideShowDialog).getList()[0].getSelected());
+                            var list = sap.ui.getCore().byId("hideShowList");
+                            var column = this.getView().byId("ShowDialogTable").getColumns();
+
+                            list.getItems().forEach(function (item, index) {
+                                column[index].setVisible(item.getSelected());
+                            });
+                            this.oColumnSelection.close();
+                        }.bind(this)
+                    }),
+                    endButton: new sap.m.Button({
+                        text: "Cancel",
+                        press: function () {
+                            this.oColumnSelection.close();
+                        }.bind(this)
+                    })
+                });
+                this.getView().addDependent(this.oColumnSelection);
+            }
+            this.oColumnSelection.open();
         }
     });
 });
